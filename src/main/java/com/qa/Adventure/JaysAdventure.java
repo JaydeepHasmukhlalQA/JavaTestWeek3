c2506c8801e459a16d0ebb355c0d9222e9247268
Jaydeep <jaydeep.hasmukhlal@academytrainee.com>
2019-05-24 09:35:31 +0100

    First init commint.

---
+++ src/main/java/com/qa/Adventure/JaysAdventure.java
@@ -0,0 +1,10 @@
+package com.qa.Adventure;
+
+public class JaysAdventure {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/test/java/come/qa/AdventureTest/JaysAdventureTest.java
@@ -0,0 +1,17 @@
+package come.qa.AdventureTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JaysAdventureTest {
+	
+	
+	
+	@Before
+	public void initBefore() {
+		
+	}
+
+}
