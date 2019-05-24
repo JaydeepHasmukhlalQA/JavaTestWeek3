0f78adb4e47e619ad503af82302ca63e157aa6d0
Jaydeep <jaydeep.hasmukhlal@academytrainee.com>
2019-05-24 12:52:08 +0100

    Commiting all work done so far.

--- src/main/java/com/qa/Adventure/JaysAdventure.java
+++ src/main/java/com/qa/Adventure/JaysAdventure.java
@@ -1,10 +1,23 @@
 package com.qa.Adventure;
 
+import java.util.Scanner;
+
 public class JaysAdventure {
 
+	public String ReadUserInput() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the player name: ");
+		return scanner.nextLine();
+	}
 	public static void main(String[] args) {
+		JaysAdventure JA = new JaysAdventure();
+		World world = new World();
 		
-
+		String playerName = JA.ReadUserInput();
+		Compass compass = new Compass("Compass", 9, 10);
+		BasicPlayer basicPlayer = new PlayerBuilder().setXPosition(0).setYPosition(0).setName(playerName).build();
+		
+		world.setPlayer(basicPlayer);
 	}
 
 }
